26cbf05cea0d1d6d552a7dffacab45c0354ac7fa
Amogh Shakya <amoghshakya@outlook.com>
2026-03-08 21:34:46 +0545

    add gases for pollution sensors

--- pollution-sensor-sim/main.py
+++ pollution-sensor-sim/main.py
@@ -33,13 +33,15 @@ pm25 = random.uniform(30, 70)
 pm10 = random.uniform(60, 140)
 temperature = random.uniform(15, 25)
 humidity = random.uniform(40, 70)
+co2 = random.uniform(400, 800)
+no2 = random.uniform(10, 50)
 
 
 def generate_sensor_data():
     # global keyword basically allows us to modify above defined variables
     # normally you wouldn't be able to do this inside a function because
     # blocke scope
-    global pm25, pm10, temperature, humidity
+    global pm25, pm10, temperature, humidity, co2, no2
 
     # simulating gradual changes in sensor readings
     # this seems more realistic than generating completely random values each time
@@ -47,25 +49,34 @@ def generate_sensor_data():
     pm10 += random.uniform(-10, 10)
     temperature += random.uniform(-0.5, 0.5)
     humidity += random.uniform(-2, 2)
+    co2 += random.uniform(-20, 20)
+    no2 += random.uniform(-5, 5)
 
     # if it's too humid, we assume particulate matters settle down
     if humidity > 70:
         pm25 *= 0.95
         pm10 *= 0.95
+        no2 *= 0.98  # NO2 also tends to decrease in high humidity
 
     # we can simulate a pollution spike randomly (small probability)
-    if random.random() < 0.08:
+    if random.random() < 0.07:
         pm25 += random.randint(10, 25)
         pm10 += random.randint(20, 80)
+        co2 += random.randint(50, 150)
+        no2 += random.randint(10, 20)
 
     # spike bhayepachi decay garaudai lyaune
     PM25_BASELINE = 80
     PM10_BASELINE = 150
+    CO2_BASELINE = 600
+    NO2_BASELINE = 30
     DECAY_RATE = 0.05
     # baseline value tira ghataudai tara 5% difference each time
     # without 5%, siddhai baseline value tira janchha, not realistic
     pm25 += (PM25_BASELINE - pm25) * DECAY_RATE
     pm10 += (PM10_BASELINE - pm10) * DECAY_RATE
+    co2 += (CO2_BASELINE - co2) * DECAY_RATE
+    no2 += (NO2_BASELINE - no2) * DECAY_RATE
 
     # upper and lower bounds to keep values from going into unrealistic ranges
     pm25 = max(5, min(pm25, 180))
@@ -78,6 +89,8 @@ def generate_sensor_data():
         "pm10": round(pm10, 2),
         "temperature": round(temperature, 2),
         "humidity": round(humidity, 2),
+        "co2": round(co2, 2),
+        "no2": round(no2, 2),
         "ts": int(time.time() * 1000),
     }
 
@@ -103,7 +116,6 @@ def send_to_thingsboard(data):
 
 # main loop here
 if __name__ == "__main__":
-    random.seed(42)
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s - %(levelname)s - %(message)s",  # time, log level, message
